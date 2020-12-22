028278aabe8707c592e9130d8a2f2d779392d4e2
seymakurtbas <seyma25kurtbas@users.noreply.github.com>
2020-12-22 16:25:31 +0300

    first commit

---
+++ homework1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+print("Lutfen 5 değer giriniz.")
+value_0 = input ("1.Deger = ")
+value_1 = input ("2.Deger = ")
+value_2 = input ("3.Deger = ")
+value_3 = input ("4.Deger = ")
+value_4 = input ("5.Deger = ")
+print ("Girilen degerler sirasiyla;")
+
+print (type (value_0))
+print (f'1.Deger: {value_0} , 2.Deger: {value_1},  3.Deger: {value_2},  4.Deger: {value_3},  5.Deger: {value_4}')
+
+print ('1.Degerin tipi : {}'.format(type ( value_0)))
+print ('2.Degerin tipi : {}'.format(type ( value_1)))
+print ('3.Degerin tipi : {}'.format(type ( value_2)))
+print ('4.Degerin tipi : {}'.format(type ( value_3)))
+print ('5.Degerin tipi : {}'.format(type ( value_4)))
+
+# Terminalden alınan value değerleri her zaman string olarak gelmektedir.
